0f6b827d45c8d29d3dc9d50d8cab853ece3e60af
Amanda Ghassaei <amandaghassaei@gmail.com>
2015-10-05 13:45:42 -0400

    quad pendulum

--- simulate_brachia_bot.m
+++ simulate_brachia_bot.m
@@ -1,37 +1,43 @@
-function simulate_double_pendulum()
+function simulate_brachia_bot()
 
     %fixed params
-    m1 = 1;
+    m1 = 0.2;
     m2 = 1;
-    m3 = 1;
+    m3 = m2;
+    m4 = m1;
     I1 = 0.05;
     I2 = 0.05;
-    I3 = 0.05;
-    l1 = 1;
-    l2 = 0.5;
-    l3 = 1;
-    c1 = 0.5;
-    c2 = 0.25;
-    c3 = 0.5;
+    I3 = I2;
+    I4 = I1;
+    l1 = 0.2;
+    l2 = 1;
+    l3 = l2;
+    l4 = l1;
+    c1 = 0.5*l1;
+    c2 = 0.5*l2;
+    c3 = 0.5*l3;
+    c4 = 0.5*l4;
     g = 9.81;
     
     th1_0 = 3;
     th2_0 = 0;
     th3_0 = 0;
+    th4_0 = 0;
     dth1_0 = 0;
     dth2_0 = 0;
     dth3_0 = 0;
+    dth4_0 = 0;
     
-    p   = [l1; l2; l3; c1; c2; c3; m1; m2; m3; I1; I2; I3; g];% parameters array
+    p = [l1; l2; l3; l4; c1; c2; c3; c4; m1; m2; m3; m4; I1; I2; I3; I4; g];% parameters array
     
     tspan = [0 10];
     inttol = 1e-6;
-    z0 = [th1_0; th2_0; th3_0; dth1_0; dth2_0; dth3_0];
+    z0 = [th1_0; th2_0; th3_0; th4_0; dth1_0; dth2_0; dth3_0; dth4_0];
     opts = odeset('AbsTol', inttol, 'RelTol', inttol);
     sol = ode45(@dynamics,tspan,z0,opts,p);
 
     %compute energy
-    E = energy_double_pendulum(sol.y, p);
+    E = energy_brachia_bot(sol.y, p);
     figure(1); clf;
     plot(sol.x,E);xlabel('Time (s)'); ylabel('Energy (J)');
     sprintf('%4.11f',max(E) - min(E));%print out max change in energy
@@ -51,7 +57,7 @@ function simulate_double_pendulum()
 %         theta1 = i/101*2*pi;
 %         for j=-100:100
 %             theta2 = j/101.0*2*pi;
-%             PotentialMat(i+101,j+101) = potential_double_pendulum(theta1, theta2, p);
+%             PotentialMat(i+101,j+101) = potential_brachia_bot(theta1, theta2, p);
 %         end
 %         thetas(i+101) = i/101*2*pi;
 %     end
@@ -70,25 +76,34 @@ end
 function dz = dynamics(t, z, p)
     
     % Get mass matrix
-    A = A_double_pendulum(z,p);
+    A = A_brachia_bot(z,p);
     
     % Get forces
-    dtheta1 = z(4);
-    dtheta2 = z(5);
-    dtheta3 = z(6);
+    th1 = z(1);
+    th2 = z(2);
+    th3 = z(3);
+    th4 = z(4);
+    
+    dth1 = z(5);
+    dth2 = z(6);
+    dth3 = z(7);
+    dth4 = z(8);
+    
     tau1 = 0;
     tau2 = 0;
     tau3 = 0;
-    u = [tau1; tau2; tau3];
-    b = b_double_pendulum(z, u, p);
+    tau4 = 0;
+    u = [tau1; tau2; tau3; tau4];
+    
+    b = b_brachia_bot(z, u, p);
     
     % Solve for qdd
     qdd = A\b;
     dz = 0*z;
     
     % Form dz
-    dz(1:3) = z(4:6);
-    dz(4:6) = qdd;
+    dz(1:4) = z(5:8);
+    dz(5:8) = qdd;
 
 end
 
@@ -98,6 +113,7 @@ function animateSol(sol,p)
     h_link1 = plot([0],[0],'LineWidth',5);
     h_link2 = plot([0],[0],'LineWidth',5);
     h_link3 = plot([0],[0],'LineWidth',5);
+    h_link4 = plot([0],[0],'LineWidth',5);
     xlabel('x')
     ylabel('y');
     h_title = title('t=0.0s');
@@ -109,11 +125,12 @@ function animateSol(sol,p)
     for t = 0:.01:sol.x(end)
         % interpolate to get state at current time.
         z = interp1(sol.x',sol.y',t)';
-        keypoints = keypoints_double_pendulum(z,p);
+        keypoints = keypoints_brachia_bot(z,p);
 
         r_B = keypoints(1:2); % Vector to joint B
         r_C = keypoints(3:4); % Vector to joint C
         r_D = keypoints(5:6); % Vector to joint D
+        r_E = keypoints(7:8); % Vector to joint E
 
         set(h_title,'String',  sprintf('t=%.2f',t) ); % update title
         
@@ -128,6 +145,10 @@ function animateSol(sol,p)
         % Plot Link 3
         set(h_link3,'XData' , [r_C(1) r_D(1)] );
         set(h_link3,'YData' , [r_C(2) r_D(2)] );
+        
+        % Plot Link 3
+        set(h_link4,'XData' , [r_D(1) r_E(1)] );
+        set(h_link4,'YData' , [r_D(2) r_E(2)] );
 
         pause(.01)
     end
