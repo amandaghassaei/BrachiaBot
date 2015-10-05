4cca389f39059b0153eef7a00c9e2b8aa2ff22fb
Amanda Ghassaei <amandaghassaei@gmail.com>
2015-10-05 15:45:26 -0400

    dynamics in separate files

--- simulate_brachia_bot.m
+++ simulate_brachia_bot.m
@@ -73,53 +73,6 @@ function simulate_brachia_bot()
 
 end
 
-function dz = dynamics(t, z, p)
-
-    %lock the wrists
-    fixed_th2 = true;
-    fixed_th4 = true;
-
-    if fixed_th2
-        z(6) = 0;
-    end
-    if fixed_th4
-        z(8) = 0;
-    end
-    
-    % Get mass matrix
-    A = A_brachia_bot(z,p);
-    
-    % Get angles
-    th1 = z(1);
-    th2 = z(2);
-    th3 = z(3);
-    th4 = z(4);
-    
-    % Get velocities
-    dth1 = z(5);
-    dth2 = z(6);
-    dth3 = z(7);
-    dth4 = z(8);
-    
-    % Forces
-    tau1 = 0;
-    tau2 = 0;
-    tau3 = 0;
-    tau4 = 0;
-    u = [tau1; tau2; tau3; tau4];
-    
-    b = b_brachia_bot(z, u, p);
-    
-    % Solve for qdd
-    qdd = A\b;
-    dz = 0*z;
-    
-    % Form dz
-    dz(1:4) = z(5:8);
-    dz(5:8) = qdd;
-
-end
-
 function animateSol(sol,p)
     % Prepare plot handles
     hold on
