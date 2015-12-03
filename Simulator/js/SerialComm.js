2713934fb33928e9a642a76acba67e96d39b7365
Amanda Ghassaei <amandaghassaei@gmail.com>
2015-12-03 01:02:12 -0500

    small changes

--- Simulator/js/SerialComm.js
+++ Simulator/js/SerialComm.js
@@ -108,7 +108,6 @@ function SerialComm(arm1, arm2, graphData){
             serialComm.trigger("change:lastMessageReceived");
             try {
                 var json = JSON.parse(data);
-                console.log(json);
                 if (json.th1 !== null && json.th1 !== undefined) {
                     var theta1 = parseFloat(json.th1);
                     if (!isNaN(theta1)){
